6bce897e87528fa2b11f5568e0f6a258ca34d2d4
EJ <harrygina09@naver.com>
2025-01-09 10:17:15 +0900

    깃허브에 겟유저추가

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,6 +8,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 //프로바이더가 전역으로 컨텍스트를 적용한다
 export const GithubProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
+  const [user, setUser] = useState([]);
   const [loading, setLoading] = useState(false);
   //키워드로 유저찾기
   const searchUsers = (text) => {
@@ -25,8 +26,27 @@ export const GithubProvider = ({ children }) => {
       })
       .catch((err) => console.log(err));
   };
+
+  //깃허브 아이디로 유저찾기
+  const getUser = (login) => {
+    setLoading(true);
+
+    fetch(`${process.env.REACT_APP_GITHUB_URL}/users${login}`, {
+      headers: {
+        Authorization: `Bearer ${GITHUB_TOKEN}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false); //데이터 로딩완료
+      })
+      .catch((err) => (window.location = "/notfound"));
+  };
   return (
-    <GithubContext.Provider value={{ users, loading, searchUsers }}>
+    <GithubContext.Provider
+      value={{ users, user, loading, searchUsers, getUser }}
+    >
       {children}
     </GithubContext.Provider>
   );
